12d8b5919784127c483c9f1d4109016422321a12
xiaoyangCui <1227687610@qq.com>
2018-11-08 16:26:16 +0800

    add db files

--- app/src/main/java/com/example/cuixiaoyang/coolweather/activity/MainActivity.java
+++ app/src/main/java/com/example/cuixiaoyang/coolweather/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.cuixiaoyang.coolweather;
+package com.example.cuixiaoyang.coolweather.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.cuixiaoyang.coolweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
